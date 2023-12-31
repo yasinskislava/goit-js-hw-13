6ffe9c112f7ea663238b813dd69f2031db59dd15
yasinskislava <yasinskislava@gmail.com>
2023-12-31 20:07:46 +0200

    abcv

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
+import {  alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+ 
+defaultModules.set(PNotifyMobile, {});
+
 const formBtn = document.getElementById("form-btn");
 const sendBtn = document.getElementById("send-btn");
 const backdrop = document.querySelector(".backdrop");
 const dataInputs = document.querySelectorAll("input");
 let data = {};
-formBtn.addEventListener("submit", () => {
-    backdrop.style.display = "none";
-});
-sendBtn.addEventListener("click", (e) => {
+formBtn.addEventListener("submit", (e) => {
     e.preventDefault();
-    backdrop.style.display = "flex";
+    alert({ text: "Форма успішно відправлена", delay: 1000 });
+    backdrop.style.display = "none";
     data = {
         name: dataInputs[0].textContent,
         email: dataInputs[1].textContent,
         comment: dataInputs[2].textContent
     };
+    
+});
+sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    backdrop.style.display = "flex";
 });
 
 export { data };
